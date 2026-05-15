6564781cc2cd4de50f82e774ae92b67ac5239421
Clarisse Paredes <clarisseparedes0729@gmail.com>
2026-05-15 16:48:40 +0800

    Add script.js file

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                purple:         '#9a849f',
+                'purple-dark':  '#9a849f',
+                cream:          '#fdf8e1',
+            },
+            fontFamily: {
+                outfit:  ['Outfit', 'sans-serif'],
+                georgia: ['Georgia', 'serif'],
+            },
+        }
+    }
+}
